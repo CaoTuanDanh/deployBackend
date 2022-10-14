8740ff78d1b90ef5a79127a5c30fba8905d4febf
CaoTuanDanh <115147816+CaoTuanDanh@users.noreply.github.com>
2022-10-14 19:03:33 +0700

    Update CheckoutService.java

--- src/main/java/com/danhthao/springbootecommerce/service/CheckoutService.java
+++ src/main/java/com/danhthao/springbootecommerce/service/CheckoutService.java
@@ -3,14 +3,7 @@ package com.danhthao.springbootecommerce.service;
 
 import com.danhthao.springbootecommerce.dto.Purchase;
 import com.danhthao.springbootecommerce.dto.PurchaseResponse;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-
-@CrossOrigin("https://guileless-pithivier-f8e66a.netlify.app/")
-@RestController
-@RequestMapping("/api/checkout")
 public interface CheckoutService {
 
     PurchaseResponse placeOrder(Purchase purchase);
